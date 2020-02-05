84707ea57d19ff120275c0cda00ec83ccd64f42f
dbhide <dhanashree.bhide3@gmail.com>
2020-02-05 16:29:23 +0530

    ADDED: Function to reset the game board

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -2,3 +2,21 @@
 
 echo "Welcome to Tic Tac Toe Game"
 
+#CONSTANTS
+ROWS=3
+COLUMNS=3
+
+declare -A board
+
+#To rest the board
+function resetBoard() {
+	for ((i=0; i<ROWS; i++))
+	do
+		for((j=0; j<COLUMNS; j++))
+		do
+			board[$i,$j]="-"
+		done
+	done
+}
+
+resetBoard
