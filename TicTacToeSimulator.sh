478384a1ce2e13195a4c82d1a085643add27821d
dbhide <dhanashree.bhide3@gmail.com>
2020-02-06 11:00:21 +0530

    ADDED: Function to display game board

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -1,10 +1,13 @@
-#!/bin/bash -x
+
+#!/bin/bash
 
 echo "Welcome to Tic Tac Toe Game"
 
 #CONSTANTS
 ROWS=3
 COLUMNS=3
+X=1
+PLAYER=1
 
 declare -A board
 
@@ -20,17 +23,38 @@ function resetBoard() {
 }
 
 #To assign letter to a player
-function symbolAndToss() {
-	if [ $((RANDOM%2)) -eq 1 ]
+function assignSymbol() {
+	if [ $((RANDOM%2)) -eq $X ]
 	then
-		echo "Player Symbol = X"
-		playerTurn=true
+		echo "Player Letter is : X"
 	else
-		echo "Player Symbol = O"
-		playerTurn=true
+		echo "Player Letter is : O"
 	fi
 }
 
-resetBoard
-symbolAndToss
+#To check who will play first
+function checkTurn() {
+	if [ $((RANDOM%2)) -eq $PLAYER ]
+	then
+		echo "Player will play first"
+	else
+		echo "Player will play first"
+	fi
+}
 
+#To display board
+function showBoard() {
+	for ((i=1; i<=ROWS; i++))
+	do
+		for ((j=1; j<=COLUMNS; j++))
+		do
+			echo -e ""-" | \c"
+		done
+		echo
+	done
+}
+
+resetBoard
+assignSymbol
+checkTurn
+showBoard
