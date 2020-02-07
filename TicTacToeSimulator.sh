1183e92ad6721ee7834947d1e236993296bf9537
dbhide <dhanashree.bhide3@gmail.com>
2020-02-07 11:47:38 +0530

    ADDED: Function to check winning positions

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Tic Tac Toe Game"
 
 #CONSTANTS
 ROWS=3
 COLUMNS=3
-X=1
 MOVES=9
 
 #Variables
@@ -13,7 +12,7 @@ counter=1
 
 declare -A board
 
-#To rest the board
+#To reset the board
 function resetBoard() {
 	for ((i=0; i<$ROWS; i++))
 	do
@@ -56,16 +55,18 @@ function showBoard() {
 	done
 }
 
+#To check valid cells to place symbol
 function validMove() {
 	row=$1
 	column=$2
 	letter=$3
-	if [[ ${board[$row,$column]} == "-" ]]
+	if [[ ${board[$row,$column]} == $"-" ]]
 	then
 		board[$row,$column]=$letter
+		showBoard
 		((counter++))
 	else
-		echo "Cell already occupied...Try another cell !!"
+		echo "Invalid Cell"
 	fi
 }
 
@@ -74,12 +75,51 @@ assignSymbol
 checkTurn
 showBoard
 
+#To check posiible winning conditions
+function winningConditions() {
+	firstDiagonalCount=0
+	secondDiagonalCount=0
+	for(( i=0; i<$ROWS; i++))
+	do
+		rowCount=0
+		columnCount=0
+		for(( j=0; j<$COLUMNS; j++))
+		do
+			if [[ ${board[$i,$j]} == $playerLetter ]]
+			then
+				rowCount=$((rowCount+1))
+			fi
+			if [[ ${board[$j,$i]} == $playerLetter ]]
+			then
+				columnCount=$((columnCount+1))
+			fi
+			if [[ $(( i+j )) -eq $(( ROWS-1 )) && ${board[$i,$j]} == $playerLetter ]]
+			then
+				secondDiagonalCount=$((secondDiagonalCount+1))
+			fi
+			if [[ $i -eq $j && ${board[$i,$j]} == $playerLetter ]]
+			then
+				firstDiagonalCount=$((firstDiagonalCount+1))
+			fi
+			if [[ $rowCount -eq $ROWS || $columnCount -eq $COLUMNS || $firstDiagonalCount -eq 3 || $secondDiagonalCount -eq 3 ]]
+			then
+				echo "Win"
+				exit
+			fi
+		done
+	done
+}
+
+#Main
 while [ $counter -ne $MOVES ]
 do
 	if [[ $player == true ]]
 	then
 		read -p "Enter row and column number - " rowValue columnValue
 		validMove $rowValue $columnValue $playerLetter
+		board[$rowValue,$columnValue]=$playerLetter
+		winningConditions
+		((counter++))
 	fi
-	showBoard
 done
+
