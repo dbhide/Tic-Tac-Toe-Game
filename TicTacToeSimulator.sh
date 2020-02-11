5e60e1c22c01737dc5303a5a9eef7a1ef42106d7
dbhide <dhanashree.bhide3@gmail.com>
2020-02-11 10:47:29 +0530

    ADDED: Function to check winning moves

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -9,7 +9,7 @@ MOVES=9
 PLAYER=0
 
 #Variables
-counter=1
+moveCounter=1
 flag=0
 
 declare -A board
@@ -45,7 +45,7 @@ function checkTurn() {
 		echo "Player will play First !!"
 	else
 		flag=1
-		echo "Computer will play First !! "
+		echo "Computer will play First !!"
 	fi
 }
 
@@ -69,10 +69,125 @@ function validMove() {
 	if [[ ${board[$row,$column]} == $"-" ]]
 	then
 		board[$row,$column]=$letter
-		showBoard
 	else
 		echo "Invalid Cell"
 		game
+	fi
+}
+
+#To check tie condition
+function checkTie() {
+	if [[ $moveCounter -gt $MOVES ]]
+	then
+		echo "It's a Tie !!"
+		exit
+	fi
+}
+
+#To check winning conditions
+function winningConditions() {
+	fl=0
+	cl=$1
+	pl=$2
+
+	if [[ ${board[0,0]} == $cl && ${board[0,1]} == $cl && ${board[0,2]} == $"-" ]]
+	then
+		board[0,2]=$pl
+
+	elif [[ ${board[0,0]} == $cl && ${board[0,2]} == $cl && ${board[0,1]} == $"-" ]]
+   then
+      board[0,1]=$pl
+
+	elif [[ ${board[0,1]} == $cl && ${board[0,2]} == $cl && ${board[0,0]} == $"-" ]]
+   then
+      board[0,0]=$pl
+
+	elif [[ ${board[1,0]} == $cl && ${board[1,1]} == $cl && ${board[1,2]} == $"-" ]]
+   then
+      board[1,2]=$pl
+
+	elif [[ ${board[1,0]} == $cl && ${board[1,2]} == $cl && ${board[1,1]} == $"-" ]]
+   then
+      board[1,1]=$pl
+
+	elif [[ ${board[1,1]} == $cl && ${board[1,2]} == $cl && ${board[1,0]} == $"-" ]]
+   then
+      board[1,0]=$pl
+
+	elif [[ ${board[2,0]} == $cl && ${board[2,1]} == $cl && ${board[2,2]} == $"-" ]]
+   then
+      board[2,2]=$pl
+
+	elif [[ ${board[2,0]} == $cl && ${board[2,2]} == $cl && ${board[2,1]} == $"-" ]]
+   then
+      board[2,1]=$pl
+
+	elif [[ ${board[2,1]} == $cl && ${board[2,2]} == $cl && ${board[2,0]} == $"-" ]]
+   then
+      board[2,0]=$pl
+
+	elif [[ ${board[0,0]} == $cl && ${board[1,0]} == $cl && ${board[2,0]} == $"-" ]]
+   then
+      board[2,0]=$pl
+
+	elif [[ ${board[0,0]} == $cl && ${board[2,0]} == $cl && ${board[1,0]} == $"-" ]]
+   then
+      board[1,0]=$pl
+
+	elif [[ ${board[1,0]} == $cl && ${board[2,0]} == $cl && ${board[0,0]} == $"-" ]]
+   then
+      board[0,0]=$pl
+
+	elif [[ ${board[0,1]} == $cl && ${board[1,1]} == $cl && ${board[2,1]} == $"-" ]]
+   then
+      board[2,1]=$pl
+
+	elif [[ ${board[0,1]} == $cl && ${board[2,1]} == $cl && ${board[1,1]} == $"-" ]]
+   then
+      board[1,1]=$pl
+
+	elif [[ ${board[1,1]} == $cl && ${board[2,1]} == $cl && ${board[0,1]} == $"-" ]]
+   then
+      board[0,1]=$pl
+
+	elif [[ ${board[0,2]} == $cl && ${board[1,2]} == $cl && ${board[2,2]} == $"-" ]]
+   then
+      board[2,2]=$pl
+
+	elif [[ ${board[0,2]} == $cl && ${board[2,2]} == $cl && ${board[1,2]} == $"-" ]]
+   then
+      board[1,2]=$pl
+
+	elif [[ ${board[1,2]} == $cl && ${board[2,2]} == $cl && ${board[0,2]} == $"-" ]]
+   then
+      board[0,2]=$pl
+
+	elif [[ ${board[0,0]} == $cl && ${board[1,1]} == $cl && ${board[2,2]} == $"-" ]]
+   then
+      board[2,2]=$pl
+
+	elif [[ ${board[0,0]} == $cl && ${board[2,2]} == $cl && ${board[1,1]} == $"-" ]]
+   then
+      board[1,1]=$pl
+
+	elif [[ ${board[1,1]} == $cl && ${board[2,2]} == $cl && ${board[0,0]} == $"-" ]]
+   then
+      board[0,0]=$pl
+
+	elif [[ ${board[0,2]} == $cl && ${board[1,1]} == $cl && ${board[2,0]} == $"-" ]]
+   then
+      board[2,0]=$pl
+
+	elif [[ ${board[0,2]} == $cl && ${board[2,0]} == $cl && ${board[1,1]} == $"-" ]]
+   then
+      board[1,1]=$pl
+
+	elif [[ ${board[1,1]} == $cl && ${board[2,0]} == $cl && ${board[0,2]} == $"-" ]]
+   then
+      board[0,2]=$pl
+
+	else
+		fl=1
 	fi
 }
 
@@ -81,8 +196,8 @@ assignSymbol
 checkTurn
 showBoard
 
-#To check posiible winning conditions
-function winningConditions() {
+#To check winner
+function winner() {
 	firstDiagonalCount=0
 	secondDiagonalCount=0
 	for(( i=0; i<$ROWS; i++))
@@ -95,18 +210,22 @@ function winningConditions() {
 			then
 				rowCount=$((rowCount+1))
 			fi
+
 			if [[ ${board[$j,$i]} == $1 ]]
 			then
 				columnCount=$((columnCount+1))
 			fi
+
 			if [[ $(( i+j )) -eq $(( ROWS-1 )) && ${board[$i,$j]} == $1 ]]
 			then
-				secondDiagonalCount=$((secondDiagonalCount+1))
+				secondDiagonalCount=$((secondDiagonalCount+1)) 
 			fi
+
 			if [[ $i -eq $j && ${board[$i,$j]} == $1 ]]
 			then
 				firstDiagonalCount=$((firstDiagonalCount+1))
 			fi
+
 			if [[ $rowCount -eq $ROWS || $columnCount -eq $COLUMNS || $firstDiagonalCount -eq 3 || $secondDiagonalCount -eq 3 ]]
 			then
 				echo $1 "Wins"
@@ -118,25 +237,32 @@ function winningConditions() {
 
 #Main
 function game() {
-while [ $counter -ne $MOVES ]
+while [ $moveCounter -ne $MOVES ]
 do
-	#read -p "Enter row and column number - " rowValue columnValue
 	if [ $flag -eq 0 ]
 	then
 		echo "Player's Turn"
-		read -p "Enter row and column number - " rowValue columnValue
+		read -p "Enter Row and Column number - " rowValue columnValue
 		validMove $rowValue $columnValue $playerLetter
 		board[$rowValue,$columnValue]=$playerLetter
-		winningConditions $playerLetter
-		((counter++))
+		winner $playerLetter
+		checkTie
+		((moveCounter++))
 		flag=1
 	else
 		echo "Computer's Turn"
-		read -p "Enter row and column number - " rowValue columnValue
-		validMove $rowValue $columnValue $computerLetter
+		winningConditions $computerLetter $computerLetter
+		if [[ $fl -eq 1 ]]
+		then
+			rowValue=$((RANDOM%3))
+			columnValue=$((RANDOM%3))
+			validMove $rowValue $columnValue $computerLetter
+		fi
+		showBoard
 		board[$rowValue,$columnValue]=$computerLetter
-		winningConditions $computerLetter
-		((counter++))
+		winner $computerLetter
+		checkTie
+		((moveCounter++))
 		flag=0
 	fi
 done
